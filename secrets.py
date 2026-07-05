65a08f32b18df1ba409ce983b04cda97d8d7db28
MIL/VUS Works <290863334+MIL-VUS-WORKS@users.noreply.github.com>
2026-07-05 14:41:08 +0100

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,10 @@
+# WIFI
+WIFI_SSID = "YOUR_SSID"
+WIFI_PASSWORD = "PASSWORD"
+
+# Location
+LAT_DOM= 38.771139 # your latitude
+LON_DOM= -9.132722 # your longitude
+
+# rayon ADS-B Flightradar
+RAYON_PLANE=10000 # viewing distance, in meter
